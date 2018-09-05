61f4ff1527a28ab41add728e56346009a0ce3017
yona <04l.yona@gmail.com>
2018-09-04 21:48:14 -0400

    Roman numerals comment

--- freeCodeCamp/toy-problems/roman-numerals.js
+++ freeCodeCamp/toy-problems/roman-numerals.js
@@ -1,13 +1,14 @@
+//arrange the numerals from greatest to least
 var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 var romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
 
 function convertToRoman(num) {
   var roman = '';
   for(var i = 0; i < numbers.length; i++) {
-    if(numbers[i] <= num) {
-      roman+= romans[i];
-      num-= numbers[i];
-      i = -1;
+    if(numbers[i] <= num) { //as long as the the current roman num is less or equal to the num
+      roman+= romans[i]; //add the roman character
+      num-= numbers[i]; //subtract the current roman num from the num
+      i = -1; //reset the index back before the beginning to get the roman character
     }
   }
  return roman;
