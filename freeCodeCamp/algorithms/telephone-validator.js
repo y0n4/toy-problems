668f2a3800f0fea416efc70a7043db8126310c96
yona <04l.yona@gmail.com>
2018-11-26 17:11:01 -0500

    Rename file to algorithms

---
+++ freeCodeCamp/algorithms/telephone-validator.js
@@ -0,0 +1,11 @@
+/*
+  return true if passed string is a valid us number
+    the number is in any type of format
+*/
+
+function telephoneCheck(str) {
+  // Good luck!
+  return true;
+}
+
+telephoneCheck("555-555-5555");
